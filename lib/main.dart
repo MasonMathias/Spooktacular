a9929726a8cc7367636200ba78ae62497368f4ad
MasonMathias <mason.mathias04@gmail.com>
2025-10-06 18:55:46 -0400

    added a moving ghost png

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -27,19 +28,53 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController c;
+  @override
+  void initState() {
+    super.initState();
+    c = AnimationController(vsync: this, duration: const Duration(seconds: 6))
+      ..repeat();
+  }
+
+  @override
+  void dispose() {
+    c.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    const w = 96.0, h = 96.0, amp = 60.0;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[           
-          ],
+      backgroundColor: Colors.black,
+      body: AnimatedBuilder(
+        animation: c,
+        builder: (_, __) => LayoutBuilder(
+          builder: (_, b) {
+            final W = b.maxWidth, H = b.maxHeight;
+            final x = (W - w) * c.value;
+            final y = H * 0.5 - h / 2 + amp * sin(2 * pi * c.value * 2);
+            return Stack(
+              children: [
+                Positioned(
+                  left: x,
+                  top: y,
+                  width: w,
+                  height: h,
+                  child: Image.asset(
+                    'assets/images/halloweenghost.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
